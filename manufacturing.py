f117e249b1e1de3e1d77c65b9a5feada0cb4c250
Alexandrea Ramnarine <alxramnarine@gmail.com>
2023-07-10 18:28:34 -0400

    init manufacturing

---
+++ manufacturing.py
@@ -0,0 +1,36 @@
+class Manufacturing:
+
+    def __init__(self, size, parts):
+        self.size = size
+        self.num_parts = parts
+        self.role_types = ['full_time', 
+                           'part_time', 
+                           'intern',
+                           'day_shift',
+                           'night_shift',
+                           'contract']
+        
+    def __str__(self):
+        return f"size: {self.size}, parts: {self.num_parts}, roles: {self.role_types}"
+
+    def vendors():
+        pass
+
+    def distributors():
+        pass
+    
+    def employee_types():
+        pass
+
+    def employees():
+        pass
+
+    def products():
+        pass
+
+    def delivery_services():
+        pass
+
+    def delivery_schedule():
+                pass
+    
