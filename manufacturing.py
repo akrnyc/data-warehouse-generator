036b79359fcee76956521fb0088cd36fa8f8651c
Alexandrea Ramnarine <alxramnarine@gmail.com>
2023-07-10 19:16:25 -0400

    add employee data

--- manufacturing.py
+++ manufacturing.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import random
+from faker import Faker
+fake = Faker()
+Faker.seed(47)
+
 class Manufacturing:
 
     def __init__(self, size, parts):
@@ -13,24 +19,34 @@ class Manufacturing:
     def __str__(self):
         return f"size: {self.size}, parts: {self.num_parts}, roles: {self.role_types}"
 
-    def vendors():
+    def vendors(self):
+        ids = []
+        names = []
+
         pass
 
-    def distributors():
+    def distributors(self):
         pass
     
-    def employee_types():
+    def employee_types(self):
+        return pd.DataFrame({'id': random.sample(range(1000, 9999), len(self.role_types)),
+                             'role_type': self.role_types})
+
+    def employees(self):
+        titles = {'Designer': 0.05, 
+                  'Engineer': 0.4, 
+                  'QA': 0.05, 
+                  'Procurement': 0.1, 
+                  'Wholesale': 0.1, 
+                  'Driver': 0.3}
+        role_ids = (self.employee_types)['id'].unique().tolist()
+
+    def products(self):
         pass
 
-    def employees():
+    def delivery_services(self):
         pass
 
-    def products():
+    def delivery_schedule(self):
         pass
-
-    def delivery_services():
-        pass
-
-    def delivery_schedule():
-                pass
     
