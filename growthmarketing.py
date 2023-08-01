2a218b0e09442705f28631784dade28eae16b987
Alexandrea Ramnarine <alxramnarine@gmail.com>
2023-08-01 18:29:51 -0400

    Update growthmarketing.py

--- growthmarketing.py
+++ growthmarketing.py
@@ -8,45 +8,74 @@ Faker.seed(47)
 
 class Growth:
     def __init__(self):
-        self.role_types = ['full_time', 
-                           'part_time', 
-                           'intern',
-                           'day_shift',
-                           'night_shift',
-                           'contract']
+        self.digital_ads = ['facebook',
+                            'google',
+                            'twitter',
+                            'adobe',
+                            'instagram']
+        
+        #https://www.quicksprout.com/best-lead-generation-companies/
+        self.partners = ['callbox',
+                         'leadgeneration',
+                         'webimax']
+        
+        self.other_ads = ['mailchimp',
+                          'directmail',
+                          'referral',
+                          'webflow']
+        
+        self.campaigns = ['holiday_sale',
+                          'seasonal_sale',
+                          'influencer',
+                          'initial_sign_up',
+                          'inactive_member']
         
     def __str__(self):
-        return f"roles: {self.role_types}"
+        return f"digital_ads: {self.digital_ads}, partners: {self.partners}, other_ads: {self.other_ads}"
     
-    def leads():
+    def digital(self):
         pass
 
-    def opportunities():
+    def digital_spend(self):
         pass
 
-    def members():
+    def affiliates(self):
         pass
 
-    def digital():
+    def affiliate_spend(self):
         pass
 
-    def digital_spend():
+    def other_ads(self):
         pass
 
-    def affiliates():
+    def other_ad_spend(self):
         pass
 
-    def affiliate_spend():
+    def leads(self, size=10):
+        addresses = [barnum.create_city_state_zip() for x in range(size)]
+        phones = [barnum.create_phone(x[0]) for x in addresses]
+        
+        return pd.DataFrame({'id': random.sample(range(1000, 9999), size),
+                             'first_name': [fake.first_name() for x in range(size)],
+                             'last_name': [fake.last_name() for x in range(size)],
+                             'street': [fake.address().split('\n')[0] for x in range(size)],
+                             'city': [x[1] for x in addresses],
+                             'state': [x[2] for x in addresses],
+                             'zipcode': [int(x[0]) for x in addresses],
+                             'email': [fake.ascii_free_email() for x in range(size)],
+                             'phone': phones,
+                             'utm_source': ,
+                             'ad_id': 
+                             })
+
+    def opportunities(self):
         pass
 
-    def other_ads():
+    def members(self):
         pass
 
-    def other_ad_spend():
+    def surveys(self):
         pass
 
-    def surveys():
-        pass
-
-    def survey_responses():
+    def survey_responses(self):
         pass
